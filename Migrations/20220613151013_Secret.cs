5330bc6c7171a537575fb3eed02524a170d176be
DenkiNoZeus <denkinozeus@gmail.com>
2022-06-16 09:24:34 -0300

    Fiz o Tutorial de API da Microsoft

---
+++ Migrations/20220613151013_Secret.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApi.Migrations
+{
+    public partial class Secret : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Secret",
+                table: "TodoItems",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Secret",
+                table: "TodoItems");
+        }
+    }
+}
